6f7f96fbdee1441b643f19b075d176af75213331
Metarion <jeffrey.markey@etudiant.univ-lille.fr>
2017-03-02 08:40:29 +0100

    t'as rien vu

---
+++ clown_database.sql
@@ -0,0 +1,79 @@
+drop table if exists media;
+drop table if exists article;
+drop table if exists projet;
+
+drop table if exists role;
+drop table if exists utilisateur;
+drop table if exists contact;
+
+-- création de la table des projets :
+-- titre obligatoire
+
+create table projet(
+	id_projet serial primary key,
+	titre varchar(200) not null,
+	description varchar(1000)
+);
+
+-- création de la table des articles :
+-- titre obligatoire
+-- relié à un projet
+
+create table article(
+	id_article serial primary key,
+	id_projet integer references projet(id_projet) 
+		on update cascade on delete restrict,
+	titre varchar(200) not null,
+	description varchar(1000),
+	contenu text not null
+);
+
+-- création de la table des média :
+-- chemin : nom du fichier dans le dossier media hebergé sur le serveur
+-- relié à un article
+
+create table media(
+	id_media serial primary key,
+	id_article integer references article(id_article) 
+ 		on update cascade on delete restrict,
+	chemin varchar(100) not null,
+	type varchar(50) not null
+);
+
+-- création de la table des utilisateurs :
+-- tous les champs obligatoires
+-- prima_pass : mot de passe crypté en sha256 + salt : 'clown'
+
+create table utilisateur(
+	id_utilisateur serial primary key,
+	nom varchar(100) not null,
+	prenom varchar(100) not null,
+	adresse_mail varchar(100) not null unique,
+	login varchar(20) not null unique,
+	prima_pass varchar(100) not null
+);
+
+-- création de la table des rôles : 
+-- 1 : inutile
+-- 2 : utilisateur
+-- 3 : inutile
+-- 4 : administrateur
+
+create table role(
+	id_utilisateur integer references utilisateur(id_utilisateur) 
+		on update cascade on delete restrict,
+	role varchar(100) default 'MatthiasLaFerme!'
+);
+
+-- création de la table de contact de l'entreprise : 
+-- coordonnées
+-- adresse civile
+
+create table contact(
+	numero_telephone varchar(10),
+	adresse_mail_contact varchar(100),
+	numero_rue varchar(6),
+	rue varchar(200),
+	code_postal varchar(5),
+	ville varchar(200)
+);
--- src/tools/Media.java
+++ src/tools/Media.java
@@ -1,18 +1,30 @@
 package tools;
 
 public class Media {
-	
+
 	private int id;
 	private String chemin, type;
-	
+	private int idArticle;
+
 	public Media(int id, String chemin, String type){
 		this.id = id;
 		this.chemin = chemin;
 		this.type = type;
 	}
-	
+
+	public Media(int id, int idArticle, String chemin, String type){
+		this.id = id;
+		this.idArticle = idArticle;
+		this.chemin = chemin;
+		this.type = type;
+	}
+
 	public int getId() {
 		return id;
+	}
+
+	public int getIdArticle() {
+		return idArticle;
 	}
 
 	public String getChemin() {
