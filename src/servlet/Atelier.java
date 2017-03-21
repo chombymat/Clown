e6ef66c85a812dc56e1ad661e5a16dd3507a9515
Metarion <jeffrey.markey@etudiant.univ-lille.fr>
2017-03-21 09:12:08 +0100

    aterliers avec id

--- src/servlet/Atelier.java
+++ src/servlet/Atelier.java
@@ -2,13 +2,14 @@ package servlet;
 
 import java.io.IOException;
 import java.util.HashMap;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import model.Modele;
+import model.ModelTmp;
 
 @SuppressWarnings("serial")
 @WebServlet("/Atelier")
@@ -16,7 +17,7 @@ public class Atelier extends HttpServlet
 {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
-		HashMap<String, tools.Article> articles = new Modele().getAteliers();
+		HashMap<Integer, tools.Article> articles = new ModelTmp().getAteliers();
 		request.setAttribute("articles", articles);
 		request.getRequestDispatcher("/WEB-INF/atelier.jsp").forward(request, response);
 	}
